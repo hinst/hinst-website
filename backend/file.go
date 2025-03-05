9927bc702dff323872e05f2d08ec209144ba6ac0
hinst <hinstance@yandex.ru>
2025-03-05 12:44:38 +0100

    Adding function writeJsonFile

--- backend/file.go
+++ backend/file.go
@@ -13,6 +13,11 @@ func readJsonFile[T any](filePath string, receiver T) T {
 	return receiver
 }
 
+func writeJsonFile[T any](filePath string, data T) {
+	var jsonBytes = assertResultError(json.Marshal(data))
+	assertError(os.WriteFile(filePath, jsonBytes, 0644))
+}
+
 func sortFilesByName(files []fs.DirEntry) {
 	slices.SortFunc(files, func(a fs.DirEntry, b fs.DirEntry) int {
 		if a.Name() < b.Name() {
