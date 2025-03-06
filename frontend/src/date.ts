7e2bcf33be2ddbdb330369767097e91ed9512687
hinst <hinstance@yandex.ru>
2025-03-06 23:46:09 +0100

    Adding functions parseMonthlyDate and getMonthName

---
+++ frontend/src/date.ts
@@ -0,0 +1,12 @@
+export function parseMonthlyDate(text: string) {
+	const parts = text.split('-');
+	return new Date(
+		parseInt(parts[0]),
+		parseInt(parts[1]) - 1,
+		1
+	);
+}
+
+export function getMonthName(date: Date) {
+	return date.toLocaleString('default', { month: 'long' });
+}
