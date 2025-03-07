be803abf1fd41d9f9a9160bbd1b546e65410b142
hinst <hinstance@yandex.ru>
2025-03-08 00:19:25 +0100

    Remove script tags

--- frontend/src/personalGoals/goalPostView.tsx
+++ frontend/src/personalGoals/goalPostView.tsx
@@ -8,19 +8,29 @@ export default function GoalPostView(props: {
 	style?: React.CSSProperties
 }) {
 	const [postData, setPostData] = useState<SmartPost | undefined>(undefined);
+	const [html, setHtml] = useState<string | undefined>(undefined);
+
 	async function load() {
 		const response = await fetch(API_URL + '/goalPost' +
 			'?goalId=' + encodeURIComponent(props.goalId) +
 			'&postDateTime=' + props.postDate);
 		const postData = await response.json();
 		setPostData(postData);
+
+		const parser = new DOMParser();
+		const parsedHTML = parser.parseFromString(postData.msg, 'text/html');
+		const scriptTags = parsedHTML.getElementsByTagName('script');
+		[...scriptTags].forEach(tag => tag.remove());
+		setHtml(parsedHTML.body.innerHTML);
 	};
+
 	useEffect(() => {
 		load();
 	}, []);
+
 	return <div style={props.style}>
-		{postData
-			? <div dangerouslySetInnerHTML={{__html: postData.msg}} />
+		{html
+			? <div dangerouslySetInnerHTML={{__html: html}} />
 			: undefined}
 	</div>;
 }
