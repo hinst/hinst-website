254c76d255759cf2a4de2af2e3642432c9a25df8
hinst <hinstance@yandex.ru>
2025-03-09 23:20:04 +0100

    Adding loading indicator for goalPostView

--- frontend/src/personalGoals/goalPostView.tsx
+++ frontend/src/personalGoals/goalPostView.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { SmartPost } from './smartPost';
 import { API_URL } from '../api';
 import SafeHtmlView from '../safeHtmlView';
@@ -8,14 +8,20 @@ export default function GoalPostView(props: {
 	postDate: string,
 	style?: React.CSSProperties
 }) {
+	const [isLoading, setIsLoading] = useState(false);
 	const [postData, setPostData] = useState<SmartPost | undefined>(undefined);
 
 	async function load() {
-		const response = await fetch(API_URL + '/goalPost' +
-			'?goalId=' + encodeURIComponent(props.goalId) +
-			'&postDateTime=' + props.postDate);
-		const postData = await response.json();
-		setPostData(postData);
+		setIsLoading(true);
+		try {
+			const response = await fetch(API_URL + '/goalPost' +
+				'?goalId=' + encodeURIComponent(props.goalId) +
+				'&postDateTime=' + props.postDate);
+			const postData = await response.json();
+			setPostData(postData);
+		} finally {
+			setIsLoading(false);
+		}
 	};
 
 	useEffect(() => {
@@ -23,6 +29,7 @@ export default function GoalPostView(props: {
 	}, []);
 
 	return <div style={props.style}>
+		{ isLoading ? <div className='ms-loading' /> : undefined }
 		{postData
 			? <SafeHtmlView htmlText={postData.msg} />
 			: undefined}
