49c312a1e4a800db9e0f5efee62fae5f11825aaf
hinst <hinstance@yandex.ru>
2025-03-11 22:33:00 +0100

    Adding function decodeJson

--- backend/json.go
+++ backend/json.go
@@ -5,3 +5,8 @@ import "encoding/json"
 func encodeJson[T any](value T) []byte {
 	return assertResultError(json.Marshal(value))
 }
+
+func decodeJson[T any](data []byte, value T) T {
+	assertError(json.Unmarshal(data, value))
+	return value
+}
