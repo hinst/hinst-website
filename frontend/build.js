d980dc4a5c451031af888690759f39a71240a1f8
hinst <hinstance@yandex.ru>
2025-03-17 20:49:04 +0100

    Adding build.js for hinst.github.io

---
+++ frontend/build.js
@@ -0,0 +1,13 @@
+import { execSync } from 'child_process';
+import fs from 'fs';
+
+const targetDirectory = '../../hinst.github.io';
+const API_URL = 'https://orangepizero2w.tail46746a.ts.net/hinst-website/api';
+
+fs.readdirSync(targetDirectory).forEach(file => {
+	const filePath = targetDirectory + '/' + file;
+	if (fs.lstatSync(filePath).isFile())
+		fs.unlinkSync(filePath);
+});
+
+execSync('npm run build -- --dist-dir=' + targetDirectory, { stdio: 'inherit', env: {API_URL} });
--- frontend/src/global.ts
+++ frontend/src/global.ts
@@ -1,3 +1,3 @@
-export const API_URL = '/hinst-website/api';
+export const API_URL = process.env.API_URL || '/hinst-website/api';
 export const COPYRIGHT_YEAR = 2025;
 export const AUTHOR_NAME = 'Alexander Savinykh';
