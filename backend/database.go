92c60e67996841319741908e579322bdad537f10
hinst <hinstance@yandex.ru>
2025-05-13 21:13:24 +0200

    Moving database functions for personal goals into a separated source file

--- backend/database.go
+++ backend/database.go
@@ -2,18 +2,12 @@ package main
 
 import (
 	"database/sql"
-	"log"
-	"math"
-	"os"
-	"path/filepath"
-	"slices"
 	"strconv"
 	"time"
 
 	_ "embed"
 
 	_ "github.com/mattn/go-sqlite3"
-	"golang.org/x/text/language"
 )
 
 // Measured in milliseconds
@@ -55,145 +49,6 @@ func (me *database) close(db *sql.DB) *sql.DB {
 
 func (me *database) getFilePath() string {
 	return me.dataDirectory + "/hinst-website.db"
-}
-
-func (me *database) getGoals() (results []goalRecord) {
-	var db = me.open()
-	defer me.close(db)
-	var rows = assertResultError(db.Query("SELECT id, title FROM goals"))
-	for rows.Next() {
-		var record goalRecord
-		assertError(rows.Scan(&record.Id, &record.Title))
-		results = append(results, record)
-	}
-	return
-}
-
-func (me *database) getGoal(goalId int64) (result goalRecord) {
-	var db = me.open()
-	defer me.close(db)
-	var row = db.QueryRow("SELECT id, title FROM goals WHERE id = ?", goalId)
-	assertError(row.Err())
-	assertError(row.Scan(&result.Id, &result.Title))
-	return
-}
-
-func (me *database) getGoalPost(goalId int64, dateTime time.Time) (result *goalPostRow) {
-	var db = me.open()
-	defer me.close(db)
-	var queryText = "SELECT * FROM goalPosts WHERE goalId = ? AND dateTime = ?"
-	var rows = assertResultError(db.Query(queryText, goalId, dateTime.UTC().Unix()))
-	if rows.Next() {
-		result = new(goalPostRow)
-		result.scan(rows)
-	}
-	return
-}
-
-func (me *database) getGoalPostImages(goalId int64, dateTime time.Time) (results []goalPostImageRow) {
-	var db = me.open()
-	defer me.close(db)
-	var queryText = "SELECT contentType, file FROM goalPostImages" +
-		" WHERE goalId = ? AND parentDateTime = ?" +
-		" ORDER BY sequenceIndex"
-	var rows = assertResultError(db.Query(queryText, goalId, dateTime.UTC().Unix()))
-	for rows.Next() {
-		var record goalPostImageRow
-		assertError(rows.Scan(&record.contentType, &record.file))
-		results = append(results, record)
-	}
-	return
-}
-
-func (me *database) getGoalPosts(goalId int64, includePrivate bool) (results []goalPostRecord) {
-	var db = me.open()
-	defer me.close(db)
-	var queryText = "SELECT goalId, dateTime, isPublic, type FROM goalPosts WHERE goalId = ?"
-	if !includePrivate {
-		queryText += " AND isPublic = 1"
-	}
-	var rows = assertResultError(db.Query(queryText, goalId))
-	for rows.Next() {
-		var record goalPostRecord
-		assertError(rows.Scan(&record.GoalId, &record.DateTime, &record.IsPublic, &record.Type))
-		results = append(results, record)
-	}
-	return
-}
-
-func (me *database) migrate() {
-	var oldDb = me.open()
-	defer me.close(oldDb)
-	me.forEachGoalPost(func(row *goalPostRow) {
-		var translatedFilesFolder = filepath.Join(me.dataDirectory, getStringFromInt64(row.goalId), "translated")
-		var translatedFileNames = assertResultError(os.ReadDir(translatedFilesFolder))
-		var minDiff int64 = math.MaxInt64
-		var matchedDateTimeText = ""
-		for _, translatedFileName := range translatedFileNames {
-			var dateTimeText = translatedFileName.Name()[0:len("2025-01-01_00-00-00")]
-			var dateTimeForFile = assertResultError(time.Parse("2006-01-02_15-04-05", dateTimeText))
-			var diff = absInt64(row.dateTime.Unix() - dateTimeForFile.Unix())
-			if diff < minDiff && diff < 60*60*12 {
-				minDiff = diff
-				matchedDateTimeText = dateTimeText
-			}
-		}
-		if matchedDateTimeText == "" {
-			log.Printf("⚠️ Cannot find translated file for row goalId=%v dateTime=%v\n", row.goalId, row.dateTime)
-			return
-		}
-		for _, supportedLanguage := range supportedLanguages[1:] {
-			var filePath = filepath.Join(translatedFilesFolder, matchedDateTimeText+"."+supportedLanguage.String()+".html")
-			if !checkFileExists(filePath) {
-				log.Printf("⚠️ Cannot find translated file for row goalId=%v dateTime=%v language=%v\n",
-					row.goalId, row.dateTime, supportedLanguage.String())
-				continue
-			}
-			var fileText = readTextFile(filePath)
-			me.setGoalPostText(row.goalId, row.dateTime, supportedLanguage, fileText)
-		}
-	})
-}
-
-func (me *database) setGoalPostPublic(row goalPostRow) {
-	var db = me.open()
-	defer me.close(db)
-	assertResultError(
-		db.Exec("UPDATE goalPosts SET isPublic = ? WHERE goalId = ? AND dateTime = ?",
-			row.isPublic, row.goalId, row.dateTime.UTC().Unix()),
-	)
-}
-
-func (me *database) setGoalPostText(goalId int64, dateTime time.Time, supportedLanguage language.Tag, text string) {
-	var db = me.open()
-	defer me.close(db)
-	assertCondition(slices.Contains(supportedLanguages, supportedLanguage),
-		func() string { return "Unsupported language: " + supportedLanguage.String() })
-	var languageName = ""
-	if supportedLanguage != supportedLanguages[0] {
-		languageName = getLanguageName(supportedLanguage)
-	}
-	var queryText = "UPDATE goalPosts SET text" + languageName + " = ? WHERE goalId = ? AND dateTime = ?"
-	var dateTimeEpoch = dateTime.UTC().Unix()
-	var result = assertResultError(db.Exec(queryText, text, goalId, dateTimeEpoch))
-	var changedRowCount = assertResultError(result.RowsAffected())
-	assertCondition(changedRowCount > 0,
-		func() string {
-			return "Cannot update translated text for goalId=" +
-				getStringFromInt64(goalId) + " dateTime=" + getStringFromInt64(dateTimeEpoch)
-		})
-}
-
-func (me *database) forEachGoalPost(callback func(row *goalPostRow)) {
-	var db = me.open()
-	defer me.close(db)
-	var rows = assertResultError(db.Query("SELECT * FROM goalPosts"))
-	for rows.Next() {
-		var row goalPostRow
-		row.scan(rows)
-		callback(&row)
-	}
-	assertError(rows.Err())
 }
 
 func (me *database) collectGarbage() {
