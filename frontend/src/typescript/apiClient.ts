14a81c5a2ae9e303b81e0f65236e012a2a8a5079
hinst <hinstance@yandex.ru>
2025-05-15 22:17:48 +0200

    Adding function ApiClient.getRiddle; Adding function fetchSafe;

--- frontend/src/typescript/apiClient.ts
+++ frontend/src/typescript/apiClient.ts
@@ -1,4 +1,5 @@
 import { GoalPostObject } from './personal-goals/smartPost';
+import { RiddleItem } from './riddle';
 import { settingsStorage } from './settings';
 
 class ApiClient {
@@ -9,38 +10,48 @@ class ApiClient {
 			options = options || {};
 			options.headers = {
 				...options.headers,
-				'Accept-Language': settingsStorage.language,
-			}
+				'Accept-Language': settingsStorage.language
+			};
 		}
-		const response = await fetch(this.url + url, options);
+		const response = await fetchSafe(this.url + url, options);
 		return response;
 	}
 
-	async goalPostSetPublic(goalId: number, postDateTime: number, isPublic: boolean): Promise<Response> {
-		const url = '/goalPost/setPublic' +
+	async goalPostSetPublic(
+		goalId: number,
+		postDateTime: number,
+		isPublic: boolean
+	): Promise<Response> {
+		const url =
+			'/goalPost/setPublic' +
 			'?goalId=' +
 			encodeURIComponent(goalId) +
 			'&postDateTime=' +
 			encodeURIComponent(postDateTime) +
 			'&isPublic=' +
 			encodeURIComponent('' + isPublic);
-		return this.fetch(url);
+		return await this.fetch(url);
 	}
 
 	async getGoalPost(goalId: number, postDateTime: number): Promise<GoalPostObject> {
-		const url = '/goalPost' +
+		const url =
+			'/goalPost' +
 			'?goalId=' +
 			encodeURIComponent(goalId) +
 			'&postDateTime=' +
 			encodeURIComponent(postDateTime);
 		const response = await this.fetch(url);
-		if (!response.ok)
-			throw new Error('Cannot load post. Status: ' + response.statusText);
-		return await response.json() as GoalPostObject;
+		return (await response.json()) as GoalPostObject;
 	}
 
-	async setGoalPostText(goalId: number, postDateTime: number, languageTag: string, text: string): Promise<Response> {
-		const url = '/goalPost/setText' +
+	async setGoalPostText(
+		goalId: number,
+		postDateTime: number,
+		languageTag: string,
+		text: string
+	): Promise<Response> {
+		const url =
+			'/goalPost/setText' +
 			'?goalId=' +
 			encodeURIComponent(goalId) +
 			'&postDateTime=' +
@@ -48,6 +59,12 @@ class ApiClient {
 			'&languageTag=' +
 			encodeURIComponent(languageTag);
 		return this.fetch(url, { method: 'POST', body: text });
+	}
+
+	async getRiddle(): Promise<RiddleItem> {
+		const url = '/riddles/new';
+		const response = await this.fetch(url);
+		return (await response.json()) as RiddleItem;
 	}
 }
 
---
+++ frontend/src/typescript/riddle.ts
@@ -0,0 +1,4 @@
+export class RiddleItem {
+	id: number = 0;
+	product: number = 0;
+}
