0cfc4fee5089ca31f581d2d6a55884e8c9674149
hinst <hinstance@yandex.ru>
2025-05-22 21:51:04 +0200

    Moving function fetchSafe into apiClient

--- frontend/src/typescript/apiClient.ts
+++ frontend/src/typescript/apiClient.ts
@@ -69,3 +69,9 @@ class ApiClient {
 }
 
 export const apiClient = new ApiClient();
+
+async function fetchSafe(url: string, requestInit: RequestInit = {}): Promise<Response> {
+	const response = await fetch(url, requestInit);
+	if (!response.ok) throw new Error(url + ' ' + response.statusText);
+	return response;
+}
--- frontend/src/typescript/fetch.ts
+++
@@ -1,5 +0,0 @@
-async function fetchSafe(url: string, requestInit: RequestInit = {}): Promise<Response> {
-	const response = await fetch(url, requestInit);
-	if (!response.ok) throw new Error(url + ' ' + response.statusText);
-	return response;
-}
