2e01fc4570b9ea450ec2fe708a1f5d6f83acc16c
hinst <hinstance@yandex.ru>
2025-09-29 22:27:00 +0200

    Deleting functions from file.go that are no longer used

--- backend/server/file.go
+++ backend/server/file.go
@@ -4,11 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"io"
-	"io/fs"
 	"os"
-	"path/filepath"
-	"slices"
-	"strings"
 
 	"github.com/hinst/hinst-website/server/file_mode"
 )
@@ -16,38 +12,8 @@ import (
 func readJsonFile[T any](filePath string, receiver T) T {
 	var file = assertResultError(os.Open(filePath))
 	defer file.Close()
-	json.NewDecoder(file).Decode(receiver)
+	assertError(json.NewDecoder(file).Decode(receiver))
 	return receiver
-}
-
-func readJsonFiles[T any](filePaths []string, threadCount int) (items []*T) {
-	type filePathItem struct {
-		filePath string
-		index    int
-	}
-	var inputs = make(chan filePathItem)
-	var ready = make(chan struct{}, threadCount)
-	items = make([]*T, len(filePaths))
-	var reader = func() {
-		for input := range inputs {
-			var item T
-			readJsonFile(input.filePath, &item)
-			items[input.index] = &item
-		}
-		ready <- struct{}{}
-	}
-	for range threadCount {
-		go reader()
-	}
-	for index, filePath := range filePaths {
-		inputs <- filePathItem{filePath, index}
-	}
-	close(inputs)
-	for range threadCount {
-		<-ready
-	}
-	close(ready)
-	return
 }
 
 func readTextFile(filePath string) string {
@@ -70,24 +36,6 @@ func writeJsonFile[T any](filePath string, data T) {
 
 func writeTextFile(filePath string, text string) {
 	assertError(os.WriteFile(filePath, []byte(text), file_mode.OS_USER_RW))
-}
-
-func sortFilesByName(files []fs.DirEntry) {
-	slices.SortFunc(files, func(a fs.DirEntry, b fs.DirEntry) int {
-		if a.Name() < b.Name() {
-			return -1
-		} else if a.Name() == b.Name() {
-			return 0
-		} else {
-			return 1
-		}
-	})
-}
-
-func getFileNameWithoutExtension(filePath string) string {
-	var fileName = filepath.Base(filePath)
-	var extension = filepath.Ext(fileName)
-	return strings.TrimSuffix(fileName, extension)
 }
 
 func checkFileExists(filePath string) bool {
