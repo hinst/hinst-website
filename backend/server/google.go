841d652968f0ee3113495f1a424444bbd455b092
hinst <hinstance@yandex.ru>
2025-10-04 17:40:47 +0200

    Connecting to Google API

--- backend/server/google.go
+++ backend/server/google.go
@@ -1,39 +1,16 @@
 package server
 
 import (
-	"golang.org/x/oauth2"
+	"context"
+
 	"golang.org/x/oauth2/google"
-	"golang.org/x/oauth2/jwt"
 )
 
-func main() {
-	// Your credentials should be obtained from the Google
-	// Developer Console (https://console.developers.google.com).
-	conf := &jwt.Config{
-		Email: "xxx@developer.gserviceaccount.com",
-		// The contents of your RSA private key or your PEM file
-		// that contains a private key.
-		// If you have a p12 file instead, you
-		// can use `openssl` to export the private key into a pem file.
-		//
-		//    $ openssl pkcs12 -in key.p12 -passin pass:notasecret -out key.pem -nodes
-		//
-		// The field only supports PEM containers with no passphrase.
-		// The openssl command will convert p12 keys to passphrase-less PEM containers.
-		PrivateKey: []byte("-----BEGIN RSA PRIVATE KEY-----..."),
-		Scopes: []string{
-			"https://www.googleapis.com/auth/bigquery",
-			"https://www.googleapis.com/auth/blogger",
-		},
-		TokenURL: google.JWTTokenURL,
-		// If you would like to impersonate a user, you can
-		// create a transport with a subject. The following GET
-		// request will be made on the behalf of user@example.com.
-		// Optional.
-		Subject: "user@example.com",
-	}
-	// Initiate an http.Client, the following GET request will be
-	// authorized and authenticated on the behalf of user@example.com.
-	client := conf.Client(oauth2.NoContext)
+const GOOGLE_SCOPE_INDEXING = "https://www.googleapis.com/auth/indexing"
+
+func connectGoogleIndexing() {
+	var jsonText = readBytesFile(requireEnvVar("GOOGLE_ACCOUNT_JSON"))
+	var conf = assertResultError(google.ConfigFromJSON(jsonText, GOOGLE_SCOPE_INDEXING))
+	client := conf.Client(context.Background(), nil)
 	client.Get("...")
 }
