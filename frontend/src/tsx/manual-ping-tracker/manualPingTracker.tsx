d85a6d280cd6331e35fd932d7a6bc921b28c7b00
hinst <hinstance@yandex.ru>
2025-10-17 21:14:39 +0200

    Adding filter: show only URLs waiting to be pinged manually

--- frontend/src/tsx/manual-ping-tracker/manualPingTracker.tsx
+++ frontend/src/tsx/manual-ping-tracker/manualPingTracker.tsx
@@ -6,30 +6,54 @@ import { Check, Copy } from 'react-feather';
 
 export default function ManualPingTracker(props: { setPageTitle: (title: string) => void }) {
 	const [urlPings, setUrlPings] = useState<Array<UrlPingRecord>>([]);
+	const [manuallyPingedVisible, setManuallyPingedVisible] = useState(true);
 	async function loadUrlPings() {
 		const urlPings = await apiClient.getUrlPings();
 		validatePings(urlPings);
 		setUrlPings(urlPings);
+	}
+	function getVisibleUrlPings() {
+		if (manuallyPingedVisible) {
+			return urlPings;
+		}
+		return urlPings.filter((ping) => ping.googlePingedManuallyAt == null);
 	}
 	useEffect(() => {
 		props.setPageTitle('Manual URL ping tracker');
 		loadUrlPings();
 	}, []);
 	return (
-		<table className='ms-table ms-striped'>
-			<thead>
-				<tr>
-					<th>URL</th>
-					<th>Google Ping</th>
-					<th>Manual Ping</th>
-				</tr>
-			</thead>
-			<tbody>
-				{urlPings.map((item) => (
-					<Row key={item.url} record={item} />
-				))}
-			</tbody>
-		</table>
+		<div>
+			<div
+				className='ms-alert ms-light'
+				style={{ display: 'flex', alignItems: 'center', gap: 10, padding: 8 }}
+			>
+				<div>Showing URLs: {getVisibleUrlPings().length}</div>
+				<b>|</b>
+				<div style={{ display: 'flex', alignItems: 'center', gap: 4 }}>
+					<input
+						type='checkbox'
+						checked={manuallyPingedVisible}
+						onChange={() => setManuallyPingedVisible(!manuallyPingedVisible)}
+					/>
+					Manually pinged
+				</div>
+			</div>
+			<table className='ms-table ms-striped'>
+				<thead>
+					<tr>
+						<th>URL</th>
+						<th>Google Ping</th>
+						<th>Manual Ping</th>
+					</tr>
+				</thead>
+				<tbody>
+					{getVisibleUrlPings().map((item) => (
+						<Row key={item.url} record={item} />
+					))}
+				</tbody>
+			</table>
+		</div>
 	);
 }
 
