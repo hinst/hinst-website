ca12f5d943aa038a4e2b224fb7a6d9ea26735a99
hinst <hinstance@yandex.ru>
2026-02-03 22:10:56 +0100

    Use search params for PersonalGoalSearch

--- frontend/src/tsx/personal-goals-search/personalGoalsSearch.tsx
+++ frontend/src/tsx/personal-goals-search/personalGoalsSearch.tsx
@@ -1,18 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { GoalPostRecord } from 'src/typescript/personal-goals/goalPostRecord';
 import { SearchBar } from './searchBar';
 import { ItemRow } from './itemRow';
 import { apiClient } from 'src/typescript/apiClient';
+import { useSearchParams } from 'react-router';
 
 export function PersonalGoalsSearch() {
 	const [items, setItems] = useState<Array<GoalPostRecord>>([]);
-	async function search(queryText: string) {
-		const items = await apiClient.searchGoalPosts(queryText);
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	function getQuery() {
+		return searchParams.get('query');
+	}
+
+	function goSearch(query: string) {
+		setSearchParams({ query: query }, { replace: true });
+	}
+
+	async function search(query: string) {
+		const items = await apiClient.searchGoalPosts(query);
 		setItems(items);
 	}
+
+	useEffect(() => {
+		const query = getQuery();
+		if (query) search(query);
+	}, [getQuery()]);
+
 	return (
 		<div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
-			<SearchBar onSearch={search} />
+			<SearchBar onSearch={goSearch} text={getQuery() || ''} />
 			<div>Results: {items.length}</div>
 			<div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
 				{items.map((item, index) => (
