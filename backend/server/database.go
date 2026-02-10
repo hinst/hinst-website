af13a82202af058de193fb7ecf215c5a59ecedbf
hinst <hinstance@yandex.ru>
2026-02-11 00:32:07 +0100

    Preparing database timeout detection

--- backend/server/database.go
+++ backend/server/database.go
@@ -2,9 +2,11 @@ package server
 
 import (
 	"context"
+	"time"
 
 	_ "embed"
 
+	"github.com/jackc/pgx/v5"
 	pgxpool "github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -15,9 +17,14 @@ type database struct {
 	pool *pgxpool.Pool
 }
 
+const databaseAcquireTimeout = 5 * time.Minute
+
+var _ pgxpool.AcquireTracer = &database{}
+
 func (me *database) init() {
 	var config = assertResultError(pgxpool.ParseConfig(requireEnvVar("POSTGRES_URL")))
 	config.MaxConns = getInt32FromString(readEnvVar("POSTGRES_MAX_CONNS", "1"))
+	config.ConnConfig.Tracer = me
 	me.pool = assertResultError(pgxpool.NewWithConfig(context.Background(), config))
 	assertResultError(me.pool.Exec(context.Background(), dbSchemaPostgre))
 }
@@ -27,4 +34,18 @@ func (me *database) close() {
 		me.pool.Close()
 		me.pool = nil
 	}
+}
+
+func (me *database) TraceQueryStart(ctx context.Context, conn *pgx.Conn, data pgx.TraceQueryStartData) context.Context {
+	return ctx
+}
+
+func (me *database) TraceQueryEnd(ctx context.Context, conn *pgx.Conn, data pgx.TraceQueryEndData) {
+}
+
+func (me *database) TraceAcquireStart(ctx context.Context, pool *pgxpool.Pool, data pgxpool.TraceAcquireStartData) context.Context {
+	return ctx
+}
+
+func (me *database) TraceAcquireEnd(ctx context.Context, pool *pgxpool.Pool, data pgxpool.TraceAcquireEndData) {
 }
