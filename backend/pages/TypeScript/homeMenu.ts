bd6fd05d2034816d6bcb12b6b7078597cc34af24
hinst <hinstance@yandex.ru>
2026-02-14 16:37:39 +0100

    Refactoring home menu

--- backend/pages/TypeScript/homeMenu.ts
+++ backend/pages/TypeScript/homeMenu.ts
@@ -3,6 +3,7 @@ const hamburgerMenuButtonInner = document.getElementById(
 	'hamburger-menu-button-inner'
 ) as HTMLDivElement;
 const hamburgerMenuPopup = document.getElementById('hamburger-menu-popup') as HTMLDivElement;
+
 let hamburgerMenuVisible = false;
 
 function updateHamburgerMenuVisibility() {
@@ -21,25 +22,32 @@ function updateHamburgerMenuVisibility() {
 	}
 }
 
+function initializeHamburgerMenu() {
+	function toggle() {
+		console.log('Hamburger menu button clicked');
+		hamburgerMenuVisible = !hamburgerMenuVisible;
+		updateHamburgerMenuVisibility();
+	}
+	hamburgerMenuButton.onclick = toggle;
+	hamburgerMenuButtonInner.onclick = toggle;
+}
+
+function highlightCurrentLanguage() {
+	const links = hamburgerMenuPopup.getElementsByTagName('a');
+	let lastLink: HTMLAnchorElement | null = null;
+	for (const link of links) {
+		const href = link.getAttribute('href') || '';
+		if (window.location.pathname.startsWith(href)) lastLink = link;
+	}
+	if (lastLink) {
+		lastLink.classList.remove('ms-outline');
+		lastLink.classList.add('ms-primary');
+	}
+}
+
 export function main() {
 	if (hamburgerMenuButton && hamburgerMenuPopup) {
-		function toggle() {
-			console.log('Hamburger menu button clicked');
-			hamburgerMenuVisible = !hamburgerMenuVisible;
-			updateHamburgerMenuVisibility();
-		}
-		hamburgerMenuButton.onclick = toggle;
-		hamburgerMenuButtonInner.onclick = toggle;
-
-		const links = hamburgerMenuPopup.getElementsByTagName('a');
-		let lastLink: HTMLAnchorElement | null = null;
-		for (const link of links) {
-			const href = link.getAttribute('href') || '';
-			if (window.location.pathname.startsWith(href)) lastLink = link;
-		}
-		if (lastLink) {
-			lastLink.classList.remove('ms-outline');
-			lastLink.classList.add('ms-primary');
-		}
+		initializeHamburgerMenu();
+		highlightCurrentLanguage();
 	}
 }
--- backend/pages/TypeScript/main.ts
+++ backend/pages/TypeScript/main.ts
@@ -1,7 +1,7 @@
 import * as goalPost from './goalPost';
 goalPost.main();
 
-import * as languageSwitcher from './languageSwitcher';
+import * as languageSwitcher from './homeMenu';
 languageSwitcher.main();
 
 import * as visitTracker from './visitTracker';
