8c083138330055d09cd08f5b1e6ccc25a60989d7
hinst <hinstance@yandex.ru>
2026-02-23 23:49:55 +0100

    Reuse header buttons

--- backend/server/template.go
+++ backend/server/template.go
@@ -5,9 +5,19 @@ import (
 	"html/template"
 )
 
+var templateFunctions = template.FuncMap{
+	"dict": func(pairs ...any) map[string]any {
+		result := make(map[string]any)
+		for i := 0; i+1 < len(pairs); i += 2 {
+			result[pairs[i].(string)] = pairs[i+1]
+		}
+		return result
+	},
+}
+
 func executeTemplateFile(filePath string, data any) string {
 	var text = readTextFile(filePath)
-	var compiledTemplate = assertResultError(template.New("page").Parse(text))
+	var compiledTemplate = assertResultError(template.New("page").Funcs(templateFunctions).Parse(text))
 	var buffer = &bytes.Buffer{}
 	assertError(compiledTemplate.Execute(buffer, data))
 	return buffer.String()
