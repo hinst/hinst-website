20db94f1791c962c15647a1819d741161f7f4b5b
hinst <hinstance@yandex.ru>
2026-06-28 18:32:40 +0200

    Refactoring performance test. Split code into three functions

--- performance-test/main.mjs
+++ performance-test/main.mjs
@@ -1,31 +1,40 @@
 //@ts-check
 import { Requests } from './requests.mjs';
 
+/**
+	@param {number[]} initialSizes
+	@param {number[]} sizes
+*/
+function assertSizes(initialSizes, sizes) {
+	for (let i = 0; i < sizes.length; i++) {
+		if (sizes[i] !== initialSizes[i]) {
+			throw new Error(
+				`Size mismatch at index ${i}: expected ${initialSizes[i]}, got ${sizes[i]}`
+			);
+		}
+	}
+}
+
+/**
+	@param {Requests} requests
+	@param {number[]} initialSizes
+*/
+async function worker(requests, initialSizes) {
+	while (true) {
+		const sizes = await requests.all();
+		assertSizes(sizes, initialSizes);
+	}
+}
+
 async function main() {
 	const requests = new Requests();
 	const initialSizes = await requests.all();
 	console.log('Initial sizes:', initialSizes);
-
 	const N = 10;
 	const workers = [];
-
 	for (let i = 0; i < N; i++) {
-		workers.push(
-			(async () => {
-				while (true) {
-					const sizes = await requests.all();
-					for (let j = 0; j < sizes.length; j++) {
-						if (sizes[j] !== initialSizes[j]) {
-							throw new Error(
-								`Size mismatch at index ${j}: expected ${initialSizes[j]}, got ${sizes[j]}`
-							);
-						}
-					}
-				}
-			})()
-		);
+		workers.push(worker(requests, initialSizes));
 	}
-
 	await Promise.all(workers);
 }
 
