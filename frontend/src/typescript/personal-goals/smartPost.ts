7afb7a9942cf1a5adeb55cf8ba2d7f813150fee5
hinst <hinstance@yandex.ru>
2026-08-09 21:30:11 +0200

    Deleting interface SmartPost because it is no longer used

--- frontend/src/typescript/personal-goals/smartPost.ts
+++ frontend/src/typescript/personal-goals/smartPost.ts
@@ -3,21 +3,6 @@ export interface SmartPostImage {
 	dataUrl: string;
 }
 
-export interface SmartPost {
-	/** Can be: 'post' */
-	type: string;
-	id: string;
-	/** Goal id */
-	obj_id: string;
-	msg: string;
-	/** Example: 2023-04-28 09:12:21 */
-	date: string;
-	comments: Comment[];
-	images: SmartPostImage[];
-	count_comments: string;
-	username: string;
-}
-
 export interface GoalPostObject {
 	goalId: number;
 	/** Unix epoch timestamp seconds */
