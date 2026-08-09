1e24e27506f68b98836d6979d146dca5269f8ca1
hinst <hinstance@yandex.ru>
2026-08-09 21:33:17 +0200

    Copying object definitions: Go -> TS

--- frontend/src/typescript/personal-goals/goalPostObject.ts
+++ frontend/src/typescript/personal-goals/goalPostObject.ts
@@ -1,14 +1,33 @@
 import { DateTime } from 'luxon';
 
+export class GoalPostObject {
+	constructor(
+		public goalId: number = 0,
+		/** Unix epoch seconds */
+		public dateTime: number = 0,
+		/** HTML */
+		public text: string = '',
+		public isAutoTranslated: boolean = false,
+		public isTranslationPending: boolean = false,
+		public languageName: string = '',
+		public languageTag: string = '',
+		public isPublic: boolean = false,
+		public searchIndexingEnabled?: boolean,
+		public imageCount: number = 0,
+	) {}
+}
+
 export class GoalPostHeader {
 	constructor(
 		public goalId: number = 0,
-		/** Unix timestamp seconds */
+		/** Unix epoch time seconds */
 		public dateTime: number = 0,
 		public isPublic: boolean = false,
+		/** "post" or "comment" */
 		public type: string = '',
 		public title: string = '',
-		public googlePingedAt: number | undefined = undefined,
+		public googlePingedAt?: number,
+		public publicUrl?: string,
 	) {}
 
 	get yearAndMonthText(): string {
