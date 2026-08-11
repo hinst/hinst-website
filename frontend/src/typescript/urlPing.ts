d7a3c37adadb365bbdb1176d49a98ac3670b5855
hinst <hinstance@yandex.ru>
2026-08-11 12:06:53 +0200

    Deleting unused class: UrlPingRecord

--- frontend/src/typescript/urlPing.ts
+++
@@ -1,7 +0,0 @@
-export class UrlPingRecord {
-	constructor(
-		public readonly url: string,
-		public readonly googlePingedAt: number | null,
-		public readonly googlePingedManuallyAt: number | null
-	) {}
-}
