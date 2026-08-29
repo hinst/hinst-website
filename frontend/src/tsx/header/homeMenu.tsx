7f6fb88feb879816700cb537cfa51108a7f4a429
hinst <hinstance@yandex.ru>
2026-08-29 14:38:03 +0200

    Adding link to manual ping tracker in admin mode

--- frontend/src/tsx/header/homeMenu.tsx
+++ frontend/src/tsx/header/homeMenu.tsx
@@ -1,7 +1,15 @@
-import { Search, Settings } from 'react-feather';
+import { Activity, Search, Settings } from 'react-feather';
 import { NavLink } from 'react-router';
+import { useEffect, useState } from 'react';
+import { apiClient } from '../../typescript/apiClient';
 
 export function HomeMenu() {
+	const [isAdminModeEnabled, setIsAdminModeEnabled] = useState(false);
+
+	useEffect(() => {
+		apiClient.isAdminModeEnabled().then(setIsAdminModeEnabled);
+	}, []);
+
 	return (
 		<div style={{ display: 'flex', gap: 5, flexDirection: 'column', width: 160 }}>
 			<NavLink
@@ -30,6 +38,21 @@ export function HomeMenu() {
 			>
 				<Search /> Search
 			</NavLink>
+			{isAdminModeEnabled && (
+				<NavLink
+					to='/manual-ping-tracker'
+					className='ms-btn ms-outline'
+					style={{
+						display: 'flex',
+						alignItems: 'center',
+						gap: 5,
+						padding: 5,
+						paddingRight: 10
+					}}
+				>
+					<Activity /> Ping Tracker
+				</NavLink>
+			)}
 		</div>
 	);
 }
