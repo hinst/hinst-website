00e8b6bb2e200b7016cd8d0e0197c92fb14a4c9f
hinst <hinstance@yandex.ru>
2026-08-30 20:21:57 +0200

    Deleting unused const API_URL

--- frontend/src/typescript/global.ts
+++ frontend/src/typescript/global.ts
@@ -1,4 +1,3 @@
-export const API_URL = process.env.API_URL || '/hinst-website/api';
 export const APP_TITLE = 'Showcase Website';
 export const COPYRIGHT_YEAR = 2025;
 export const AUTHOR_NAME = 'Alexander Savinykh';
