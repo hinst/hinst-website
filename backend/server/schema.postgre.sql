f00d6a5257dff4e9093fa88fdcb2ab4d255b14c9
hinst <hinstance@yandex.ru>
2026-09-12 12:07:39 +0200

    Trimming comments

--- backend/server/schema.postgre.sql
+++ backend/server/schema.postgre.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE IF NOT EXISTS goals (
 	id BIGINT NOT NULL PRIMARY KEY,
-	title TEXT[] NOT NULL DEFAULT '{}', /* [russian, english, german] */
+	title TEXT[] NOT NULL DEFAULT '{}',
 	description TEXT NOT NULL, /* HTML */
 	authorName TEXT NOT NULL,
 
@@ -15,9 +15,9 @@ CREATE TABLE IF NOT EXISTS goalPosts (
 	dateTime BIGINT NOT NULL, /* Unix seconds UTC */
 	isPublic BOOLEAN NOT NULL DEFAULT FALSE,
 	searchIndexingEnabled BOOLEAN NOT NULL DEFAULT FALSE,
-	text TEXT[] NOT NULL DEFAULT '{}',  /* HTML, [russian, english, german] */
+	text TEXT[] NOT NULL DEFAULT '{}',  /* HTML */
 	type TEXT NOT NULL,
-	title TEXT[] NOT NULL DEFAULT '{}',  /* [russian, english, german] */
+	title TEXT[] NOT NULL DEFAULT '{}',
 	googlePingedAt BIGINT NOT NULL DEFAULT 0, /* Unix seconds UTC, 0 means never pinged */
 	googleSearchIndexingStatus TEXT NOT NULL DEFAULT '',
 	googleSearchIndexingStatusCheckedAt BIGINT NOT NULL DEFAULT 0, /* Unix seconds UTC */
